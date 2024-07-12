823395168a7f54a07c91ac6847c4de43db50f6eb
zhaotyue <68317664+zhaotyue@users.noreply.github.com>
2024-07-12 09:24:25 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # 端口参数 （必填）
-export WEBPORT=
-export VMPORT=
+export WEBPORT=6705
+export VMPORT=20045
 
 # web.js 参数 （必填）
-export UUID=de04add9-5c68-8bab-950c-08cd5320df18
+export UUID=3cdbeeb6-ade7-44f9-8192-c345fb710214
 export WSPATH=serv00
 
 # ARGO 隧道参数（如需固定 ARGO 隧道，请把 ey 开头的 ARGO 隧道的 token 填入 ARGO_AUTH ，仅支持这一种方式固定，隧道域名代理的协议为 HTTP ，端口为 VMPORT 同端口。如果不固定 ARGO 隧道，请删掉ARGO_DOMAIN那行，保留ARGO_AUTH这行。）
-export ARGO_AUTH=''
-export ARGO_DOMAIN=
+export ARGO_AUTH=''eyJhIjoiM2I3Zjk2MWZiYjIwZjgzOTgyYTM3OWEwOWQ4NDkyYjIiLCJ0IjoiYzIyZDU5NTItYjg5NC00ZGViLWJmNTQtOTMwNWQ1MGM4YTM3IiwicyI6Ik1ETmpOVEZqTlRNdE5HUTJaUzAwTXpVNUxUaGhOV0l0T0RjME5UUTBaRFk0T1RZMSJ9"
+export ARGO_DOMAIN=tunnel00.host.at.eu.org
 
 # 网页的用户名和密码（可不填，默认为 admin 和 password ，如果不填请删掉这两行）
-export WEB_USERNAME=
-export WEB_PASSWORD=
+export WEB_USERNAME=root
+export WEB_PASSWORD=5006
 
 # 启动程序
 USERNAME=$(whoami)
